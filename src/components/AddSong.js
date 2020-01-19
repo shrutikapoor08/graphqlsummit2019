8877c35f6cf7fd966be4fcdc7a93cf50873d9082
Shruti Kapoor <shruti.kapoor08@gmail.com>
2020-01-18 19:37:53 -0800

    Changing to required

--- src/components/AddSong.js
+++ src/components/AddSong.js
@@ -1,51 +1,82 @@
 import React, { useState, useContext } from "react";
 import { Mutation } from "react-apollo";
-import { ADD_SONG } from '../graphql/mutations'
+import { ADD_SONG } from "../graphql/mutations";
 import Container from "muicss/lib/react/container";
-import Context from '../context';
-
+import Context from "../context";
 
 const AddSong = () => {
-    const [name, setName] = useState("");
-    const [artist, setArtist] = useState("");
-    const [lyrics, setLyrics] = useState("");
-    const {dispatch} = useContext(Context);
+  const [name, setName] = useState("");
+  const [artist, setArtist] = useState("");
+  const [lyrics, setLyrics] = useState("");
+  const { dispatch } = useContext(Context);
 
-    return (
-        <Mutation mutation={ADD_SONG}>
-            {(addSong) => (
-        <form className={'mui-form'} onSubmit={e => {
+  return (
+    <Mutation mutation={ADD_SONG}>
+      {addSong => (
+        <form
+          className={"mui-form"}
+          onSubmit={e => {
             e.preventDefault();
-            addSong({ variables: { song: {
-                        name,
-                        artist,
-                        lyrics
-                        }}
-                    });
-            const songs = [{name, artist, lyrics}];
-            dispatch({type: "ADD_CONTENT", payload: songs});
+            addSong({
+              variables: {
+                song: {
+                  name,
+                  artist,
+                  lyrics
+                }
+              }
+            });
+            const songs = [{ name, artist, lyrics }];
+            dispatch({ type: "ADD_CONTENT", payload: songs });
             setName("");
             setLyrics("");
             setArtist("");
-        }}>
-                <legend>Add a new song</legend>
-                <div className={'mui-textfield mui-textfield--float-label width-small'}>
-                    <input value={name} type="text" name="name" onChange={e => setName(e.target.value)} />
-                    <label>Name</label>
-                </div>
-                <div className="mui-textfield mui-textfield--float-label width-small">
-                <input value={artist} type="text" name="artist" onChange={e => setArtist(e.target.value)}/>
-                    <label>Artist</label>
-                </div>
-                <div className="mui-textfield mui-textfield--float-label">
-                    <textarea value={lyrics} type="text" name="lyrics" onChange={e => setLyrics(e.target.value)}/>
-                    <label>Lyrics</label>
-                </div>
-            <button className={`mui-btn mui-btn--raised mui-btn--primary`} type="submit"> Add Song </button>
+          }}
+        >
+          <legend>Add a new song</legend>
+          <div
+            className={"mui-textfield mui-textfield--float-label width-small"}
+          >
+            <input
+              value={name}
+              required
+              type="text"
+              name="name"
+              onChange={e => setName(e.target.value)}
+            />
+            <label>Name</label>
+          </div>
+          <div className="mui-textfield mui-textfield--float-label width-small">
+            <input
+              value={artist}
+              required
+              type="text"
+              name="artist"
+              onChange={e => setArtist(e.target.value)}
+            />
+            <label>Artist</label>
+          </div>
+          <div className="mui-textfield mui-textfield--float-label">
+            <textarea
+              value={lyrics}
+              required
+              type="text"
+              name="lyrics"
+              onChange={e => setLyrics(e.target.value)}
+            />
+            <label>Lyrics</label>
+          </div>
+          <button
+            className={`mui-btn mui-btn--raised mui-btn--primary`}
+            type="submit"
+          >
+            {" "}
+            Add Song{" "}
+          </button>
         </form>
-            )}
-        </Mutation>
-    )
-}
+      )}
+    </Mutation>
+  );
+};
 
-export default AddSong
+export default AddSong;
